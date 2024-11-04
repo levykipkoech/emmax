d1bd60bc299fdc00ef9e2475ebdbdb8831c03cc6
kipko <kipkoechlevy096@gmail.com>
2024-11-04 17:15:30 +0300

    created login component

---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+        <div className='bg '>
+            <input className='bg-gray-200' type='email' placeholder='jondoe@gmail.com'/>
+            <input className='bg-gray-200' type='password'/>
+        </div>
+    </div>
+  )
+}
--- app/productform/page.js
+++ app/productform/page.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Fugaz_One, Rubik_Wet_Paint } from 'next/font/google';
+import React from 'react';
+
+const rubik = Rubik_Wet_Paint({ subsets: ['latin'], weight: ['400'] });
+const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 
 export default function ProductForm() {
   return (
-    <div> ProductForm</div>
-  )
+    <div className='h-screen'>
+       <h1
+          className={
+            'font-bold uppercase text-center text-base md:text-4xl p-2 text-[#48F2FB] ' +
+            rubik.className
+          }
+        >
+          emmax
+        </h1>
+      <div className="flex justify-center capitalize  ">
+       
+        <form className="flex flex-col gap-3 shadow-md shadow-gray-400 rounded-xl p-4">
+          <h3 className={"text-xl sm:text-3xl " + fugaz.className}>create your product</h3>
+          <lebel>name</lebel>
+          <input className='bg-gray-200 rounded-xl' />
+          <lebel>buying price</lebel>
+          <input className='bg-gray-200 rounded-xl' />
+          <lebel>selling price</lebel>
+          <input className='bg-gray-200 rounded-xl' />
+          <lebel>quantity</lebel>
+          <input  className='bg-gray-200 rounded-xl'/>
+         
+
+          <input
+          type='submit'
+          className='rounded-full bg-green-200 hover:bg-green-500 hover:scale-105 ease-in duration-300 shadow-md shadow-gray-300 mt-3 p-2'
+          value='create product'
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
