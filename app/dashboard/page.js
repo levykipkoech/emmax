cb4952be64db27d0b4b52c1edc694ffb12a7be36
kipko <kipkoechlevy096@gmail.com>
2024-11-30 18:51:36 +0300

    dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -5,12 +5,11 @@ import Navbar from '../(components)/Navbar';
 export default function dashboardPage() {
   return (
     <div>
-      <div>
+      <div className='sticky top-2 h-10'>
         <Navbar />
-        
       </div>
       <div className="h-screen ">
-        
+        <h1 className='capitalize tracking-widest font-extrabold text-center text-4xl text-orange-400'>welcome to the dashboard</h1>
       </div>
     </div>
   );
