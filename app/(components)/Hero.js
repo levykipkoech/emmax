7849310c78c7c3fcf0dce5653aa4608dbf4bcb45
kipko <kipkoechlevy096@gmail.com>
2024-12-11 23:07:46 +0300

    added some styling

--- app/(components)/Hero.js
+++ app/(components)/Hero.js
@@ -9,14 +9,14 @@ const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 export default function Hero() {
   return (
     <div className='h-screen'>
-    <div className="flex flex-col flex-1 pt-8 w-full justify-between place-items-center">
+    <div className="flex flex-col flex-1 pt-8 w-full justify-between place-items-center m-3">
       <h1
         className={
-          'uppercase tracking-widest text-center text-3xl bg-gradient-to-r from-orange-400 to-gray-300 bg-clip-text text-transparent mt-12 sm:text-5xl ' +
+          'uppercase tracking-widest text-center text-3xl bg-gradient-to-r from-orange-400 to-gray-300 bg-clip-text text-transparent  sm:text-5xl ' +
           rubik.className
         }
       >
-        <span className="p-4 ">e</span>
+        <span className="p-4 m-4">e</span>
         <span className="p-4 ">m</span>
         <span className="p-4 ">m</span>
         <span className="p-4 ">a</span>
@@ -24,7 +24,7 @@ export default function Hero() {
       </h1>
      
       <div className='pt-12 '>
-        <p className={'text-xl sm:text-2xl capitalize tracking-widest text-white ' + fugaz.className }>your stock management made easy</p>
+        <p className={'text-xl m-3 sm:text-2xl capitalize tracking-widest text-white ' + fugaz.className }>your stock management made easy</p>
       </div>
       
     </div>
