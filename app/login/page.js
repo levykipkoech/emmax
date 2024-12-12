1bfa5088f1489cfdec2c5a2879c91df926db06f7
kipko <kipkoechlevy096@gmail.com>
2024-12-12 16:58:39 +0300

    correctly arrabged the ui

--- app/login/page.js
+++ app/login/page.js
@@ -27,7 +27,9 @@ export default function Page() {
 
   async function handleSubmit() {
     if (!email || !password || password.length < 5) {
-      setError('Please provide a valid email and a password of at least 5 characters.');
+      setError(
+        'Please provide a valid email and a password of at least 5 characters.'
+      );
       return;
     }
 
@@ -50,27 +52,24 @@ export default function Page() {
   }
 
   return (
-    <div>
-      <Link href="/">
+    <div className="h-screen flex flex-col items-center justify-center px-4">
+      <Link href="/" className="absolute top-6 left-6">
         <h1
-          className={
-            'bg-gradient-to-r from-orange-500 to-gray-300 bg-clip-text text-transparent uppercase font-bold pl-3 text-3xl fixed ' +
-            rubik.className
-          }
+          className={`bg-gradient-to-r from-orange-500 to-gray-300 bg-clip-text text-transparent uppercase font-bold py-3 text-3xl ${rubik.className}`}
         >
           emmax
         </h1>
       </Link>
-      <div className="flex-1 flex flex-col justify-center items-center gap-4 p-3 text-xl">
-        <h3 className={'text-5xl md:text-6xl ' + fugaz.className}>
-          {isRegistered ? 'Register ' : 'Log in'}
+      <div className="flex flex-col justify-center items-center gap-8 w-full max-w-[400px]">
+        <h3 className={`text-5xl md:text-6xl ${fugaz.className}`}>
+          {isRegistered ? 'Register' : 'Log in'}
         </h3>
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           aria-label="email"
-          className="w-full max-w-[400px] mx-auto px-4 py-2 duration-300 hover:border-gray-600 focus:border-gray-600 sm:py-3 border-solid border-2 border-gray-400 rounded-lg outline-none"
+          className="w-full px-4 py-2 sm:py-3 border-2 border-solid border-gray-400 rounded-lg outline-none duration-300 hover:border-gray-600 focus:border-gray-600"
         />
         <input
           value={password}
@@ -78,28 +77,17 @@ export default function Page() {
           placeholder="Password"
           type="password"
           aria-label="password"
-          className="w-full max-w-[400px] mx-auto px-4 py-2 duration-300 hover:border-gray-600 focus:border-gray-600 sm:py-3 border-solid border-2 border-gray-400 rounded-lg outline-none"
+          className="w-full px-4 py-2 sm:py-3 border-2 border-solid border-gray-400 rounded-lg outline-none duration-300 hover:border-gray-600 focus:border-gray-600"
         />
-        {isRegistered && (
-          <select
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-            aria-label="role"
-            className="w-full max-w-[400px] mx-auto px-4 py-2 duration-300 hover:border-gray-600 focus:border-gray-600 sm:py-3 border-solid border-2 border-gray-400 rounded-lg outline-none"
-          >
-            <option value="user">user</option>
-            <option value="admin">admin</option>
-          </select>
-        )}
         <button
           onClick={handleSubmit}
           disabled={authenticating}
-          className="rounded-full bg-orange-300 hover:bg-orange-600 hover:scale-105 ease-in duration-300 shadow-md shadow-gray-300 mt-3 p-2 max-w-[400px] w-full mx-auto"
+          className="w-full mt-2 p-2 rounded-full bg-orange-300 shadow-md shadow-gray-300 hover:bg-orange-600 hover:scale-105 ease-in duration-300"
         >
           {authenticating ? 'Submitting...' : 'Submit'}
         </button>
         {error && <p className="text-red-500">{error}</p>}
-        <div className={'text-lg md:text-2xl ' + fugaz.className}>
+        <div className={`text-lg md:text-2xl ${fugaz.className}`}>
           <p>
             {isRegistered
               ? 'Already have an account? '
